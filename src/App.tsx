26fc3744e8eb404fbc25bb69c42f98307c54fc18
brunoalbrito <brunoalbuquerquebrito@outlook.com>
2023-08-11 21:20:12 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+// import { useState } from 'react'
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="container mx-auto">
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <button className="text-white bg-blue-500 rounded-lg px-5 py-5 text-lg">
+          Practice Hiragana
+        </button>
+
+        <button className="text-white bg-blue-500 rounded-lg px-5 py-5 text-lg">
+          Practice Hiragana
+        </button>
+
+        <button className="text-white bg-blue-500 rounded-lg px-5 py-5 text-lg">
+          Practice Kanji
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default App;
