19e0c0269dc781a1b0bbe0fa047750473131ae63
brunoalbrito <brunoalbuquerquebrito@outlook.com>
2023-08-11 22:16:13 -0300

    Static deploy

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ const Button: React.FC<ButtonProperties> = ({
   if (isSelected) {
     return (
       <button
-        className="text-white bg-blue-500 rounded-lg px-5 py-5 text-l w-full"
+        className="text-white bg-blue-500 border-2 border-blue-500 rounded-lg px-5 py-5 text-lg w-full"
         onClick={onClick}
       >
         {children}
@@ -45,33 +45,35 @@ function App() {
   return (
     <>
       <div className="container mx-auto">
-        <div className="flex flex-row justify-center space-x-4">
-          <Button
-            isSelected={selectedOption === KanaOptions.Hiragana}
-            onClick={() => {
-              setSelectedOption(KanaOptions.Hiragana);
-            }}
-          >
-            Practice Hiragana
-          </Button>
-
-          <Button
-            isSelected={selectedOption === KanaOptions.Katakana}
-            onClick={() => {
-              setSelectedOption(KanaOptions.Katakana);
-            }}
-          >
-            Practice Katakana
-          </Button>
-
-          <Button
-            isSelected={selectedOption === KanaOptions.AllKana}
-            onClick={() => setSelectedOption(KanaOptions.AllKana)}
-          >
-            Practice All Kana
-          </Button>
-
-          {/* {selectedOption === KanaOptions.Hiragana} */}
+        <div className="grid grid-col-4 grid-flow-row gap-4">
+          <div className="col-span-2">
+            <Button
+              isSelected={selectedOption === KanaOptions.Hiragana}
+              onClick={() => {
+                setSelectedOption(KanaOptions.Hiragana);
+              }}
+            >
+              Practice Hiragana
+            </Button>
+          </div>
+          <div className="col-span-2">
+            <Button
+              isSelected={selectedOption === KanaOptions.Katakana}
+              onClick={() => {
+                setSelectedOption(KanaOptions.Katakana);
+              }}
+            >
+              Practice Katakana
+            </Button>
+          </div>
+          <div className="col-span-4">
+            <Button
+              isSelected={selectedOption === KanaOptions.AllKana}
+              onClick={() => setSelectedOption(KanaOptions.AllKana)}
+            >
+              Practice All Kana
+            </Button>
+          </div>
         </div>
       </div>
     </>
