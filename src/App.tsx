8b0e2454839bd3aa93e48e3f98596bb5ef857e7e
brunoalbrito <brunoalbuquerquebrito@outlook.com>
2023-08-11 21:58:08 -0300

    Including options kana buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,78 @@
-// import { useState } from 'react'
-
+import { useState } from "react";
 import "./App.css";
 
+enum KanaOptions {
+  Hiragana,
+  Katakana,
+  AllKana,
+}
+
+interface ButtonProperties {
+  isSelected: boolean;
+  children: React.ReactNode;
+  onClick(): void;
+}
+
+const Button: React.FC<ButtonProperties> = ({
+  isSelected,
+  children,
+  onClick,
+}) => {
+  if (isSelected) {
+    return (
+      <button
+        className="text-white bg-blue-500 rounded-lg px-5 py-5 text-l w-full"
+        onClick={onClick}
+      >
+        {children}
+      </button>
+    );
+  }
+
+  return (
+    <button
+      className="text-blue-500 bg-white border-2 border-blue-500 rounded-lg px-5 py-5 text-lg w-full"
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+};
+
 function App() {
+  const [selectedOption, setSelectedOption] = useState(KanaOptions.Hiragana);
+
   return (
     <>
       <div className="container mx-auto">
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
-        <button className="text-white bg-blue-500 rounded-lg px-5 py-5 text-lg">
-          Practice Hiragana
-        </button>
+        <div className="flex flex-row justify-center space-x-4">
+          <Button
+            isSelected={selectedOption === KanaOptions.Hiragana}
+            onClick={() => {
+              setSelectedOption(KanaOptions.Hiragana);
+            }}
+          >
+            Practice Hiragana
+          </Button>
 
-        <button className="text-white bg-blue-500 rounded-lg px-5 py-5 text-lg">
-          Practice Hiragana
-        </button>
+          <Button
+            isSelected={selectedOption === KanaOptions.Katakana}
+            onClick={() => {
+              setSelectedOption(KanaOptions.Katakana);
+            }}
+          >
+            Practice Katakana
+          </Button>
 
-        <button className="text-white bg-blue-500 rounded-lg px-5 py-5 text-lg">
-          Practice Kanji
-        </button>
+          <Button
+            isSelected={selectedOption === KanaOptions.AllKana}
+            onClick={() => setSelectedOption(KanaOptions.AllKana)}
+          >
+            Practice All Kana
+          </Button>
+
+          {/* {selectedOption === KanaOptions.Hiragana} */}
+        </div>
       </div>
     </>
   );
